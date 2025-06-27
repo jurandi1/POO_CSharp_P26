c59271f747cd783f228d4a3fad3d9248dc95bbea
Jurandi Junior <jurandijr20@gmail.com>
2025-06-27 18:11:08 -0300

    Uma experiência com Comparison

--- Program.cs
+++ Program.cs
@@ -1,6 +1,20 @@
-﻿internal class Program
+﻿using POO_CSharp_P26.Entities;
+
+internal class Program
 {
     private static void Main(string[] args)
     {
+        List<Product> list = new List<Product>();
+
+        list.Add(new Product("TV", 900.00));
+        list.Add(new Product("Notebook", 1200.00));
+        list.Add(new Product("Tablet", 450.00));
+
+        list.Sort((p1, p2) => p1.Name.ToUpper().CompareTo(p2.Name.ToUpper()));
+
+        foreach (Product p in list)
+        {
+            Console.WriteLine(p);
+        }
     }
 }
